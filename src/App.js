571a490a28726e02798573c34f65a3d039394bfc
Fakhruddin Ali Ahmad <ali.adravi@gmail.com>
2024-07-10 13:46:47 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import About from './pages/about'
+import Product from './pages/products'
+import { useState } from 'react';
 function App() {
+
+  let [isVisible, setIsVisible] = useState(false)
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <About />
+        {/* {
+           isVisible === true? <Product /> : null
+        } */}
+        <Product />
+        
+        <button onClick={() => setIsVisible(!isVisible)}>Toggle</button>
+       
+
       </header>
     </div>
   );
